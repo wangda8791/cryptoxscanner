67477b97a0deb6214d1f240537f4f458687991bc
Cranky Kernel <thecrankykernel@gmail.com>
2018-12-09 23:27:15 -0600

    dev.sh: ignore packr output

--- dev.sh
+++ dev.sh
@@ -5,6 +5,6 @@ trap 'echo "Killing background jobs..."; kill $(jobs -p)' EXIT
 (cd webapp && make update-build && npm start) &
 
 while true; do
-    find go/* -name \*.go | \
+    find go/* -name \*.go | grep -v packr.go | \
 	entr -d -r sh -c "cd go && make && ./cryptoxscanner server"
 done
