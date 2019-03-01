2e70f35bf65c7b57e029c726013001c58e1cc4b5
Cranky Kernel <thecrankykernel@gmail.com>
2019-02-28 22:58:54 -0600

    fixup

---
+++ build/build.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+docker build -t cryptoxscanner-builder -f build/Dockerfile .
+mkdir -p .docker_cache
+docker run --rm -it \
+       -v `pwd`:/src \
+       -v `pwd`/.docker_cache/node_modules:/src/webapp/node_modules \
+       -v `pwd`/.docker_cache/go:/home/builder/go \
+       -w /src \
+       -e REAL_UID=`id -u` -e REAL_GID=`id -g` \
+       cryptoxscanner-builder "make install-deps dist"
